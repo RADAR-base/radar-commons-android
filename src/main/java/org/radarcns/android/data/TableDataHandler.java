734a1cbd2ee2cecfa0fea5aa9a3cd401fdf63795
Joris Borgdorff <joris@thehyve.nl>
2017-04-19 10:19:45 +0200

    Also deregister connectivity receiver

--- src/main/java/org/radarcns/android/data/TableDataHandler.java
+++ src/main/java/org/radarcns/android/data/TableDataHandler.java
@@ -222,6 +222,7 @@ public class TableDataHandler implements DataHandler<MeasurementKey, SpecificRec
             if (isStarted()) {
                 stop();
             }
+            context.unregisterReceiver(connectivityReceiver);
         }
     }
 
