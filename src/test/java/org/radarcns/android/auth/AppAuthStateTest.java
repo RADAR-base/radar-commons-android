c3ba4c7cac0797b80f650dee6af155b8c37cc53b
Joris Borgdorff <joris@thehyve.nl>
2017-12-07 16:18:47 +0100

    More tests and error handling

---
+++ src/test/java/org/radarcns/android/auth/AppAuthStateTest.java
@@ -0,0 +1,78 @@
+package org.radarcns.android.auth;
+
+import android.content.Context;
+import android.os.Bundle;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.radarcns.android.auth.LoginManager.AUTH_TYPE_BEARER;
+import static org.radarcns.android.auth.portal.ManagementPortalClient.MP_REFRESH_TOKEN_PROPERTY;
+import static org.radarcns.android.auth.portal.ManagementPortalClient.SOURCES_PROPERTY;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class AppAuthStateTest {
+    private AppAuthState state;
+    private ArrayList<AppSource> sources;
+
+    @Before
+    public void setUp() {
+        sources = new ArrayList<>();
+        AppSource source = new AppSource(1, "radar", "test", "1.0", true);
+        source.setExpectedSourceName("something");
+        sources.add(source);
+
+        state = new AppAuthState.Builder()
+                .token("abcd")
+                .tokenType(AUTH_TYPE_BEARER)
+                .projectId("p")
+                .userId("u")
+                .property(MP_REFRESH_TOKEN_PROPERTY, "efgh")
+                .property(SOURCES_PROPERTY, sources)
+                .header("Authorization", "Bearer abcd")
+                .expiration(System.currentTimeMillis() + 10_000L)
+                .build();
+
+        testProperties(state);
+    }
+
+    @Test
+    public void addToPreferences() throws Exception {
+        Context context = RuntimeEnvironment.application.getApplicationContext();
+        state.addToPreferences(context);
+
+        AppAuthState readState = AppAuthState.Builder.from(context).build();
+        testProperties(readState);
+    }
+
+    private void testProperties(AppAuthState state) {
+        assertEquals("abcd", state.getToken());
+        assertEquals("efgh", state.getProperty(MP_REFRESH_TOKEN_PROPERTY));
+        assertEquals("p", state.getProjectId());
+        assertEquals("u", state.getUserId());
+        assertTrue(state.isValidFor(9, TimeUnit.SECONDS));
+        assertFalse(state.isValidFor(11, TimeUnit.SECONDS));
+        assertEquals(AUTH_TYPE_BEARER, state.getTokenType());
+        assertEquals("Bearer abcd", state.getHeaders().get(0).getValue());
+        assertEquals(sources, state.getProperty(SOURCES_PROPERTY));
+    }
+
+    @Test
+    public void addToBundle() {
+        Bundle bundle = new Bundle();
+        state.addToBundle(bundle);
+
+        AppAuthState readState = AppAuthState.Builder.from(bundle).build();
+        testProperties(readState);
+    }
+}
