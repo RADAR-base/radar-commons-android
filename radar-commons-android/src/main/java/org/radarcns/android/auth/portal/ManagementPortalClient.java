d951345f24fd770dd899e7e89e172690981a8a85
nivethika <nivemaham@gmail.com>
2018-08-13 18:14:28 +0200

    add new property keys

--- radar-commons-android/src/main/java/org/radarcns/android/auth/portal/ManagementPortalClient.java
+++ radar-commons-android/src/main/java/org/radarcns/android/auth/portal/ManagementPortalClient.java
@@ -30,6 +30,8 @@ public class ManagementPortalClient implements Closeable {
     public static final String SOURCES_PROPERTY =
             ManagementPortalClient.class.getName() + ".sources";
     public static final String MP_REFRESH_TOKEN_PROPERTY = ManagementPortalClient.class.getName() + ".refreshToken";
+    public static final String PRIVACY_POLICY_URL_PROPERTY = ManagementPortalClient.class.getName() + ".privacyPolicyUrl";
+    public static final String BASE_URL_PROPERTY = ManagementPortalClient.class.getName() + ".baseUrl";
     private static final String APPLICATION_JSON = "application/json";
     private static final String APPLICATION_JSON_UTF8 = APPLICATION_JSON + "; charset=utf-8";
     private static final MediaType APPLICATION_JSON_MEDIA_TYPE = MediaType
