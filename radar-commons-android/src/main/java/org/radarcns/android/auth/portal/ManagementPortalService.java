b7bf19b42e5b70f4620ac32262a8826e370d131b
nivethika <nivemaham@gmail.com>
2018-08-13 18:15:22 +0200

    get refreshToken implementation

--- radar-commons-android/src/main/java/org/radarcns/android/auth/portal/ManagementPortalService.java
+++ radar-commons-android/src/main/java/org/radarcns/android/auth/portal/ManagementPortalService.java
@@ -48,12 +48,17 @@ public class ManagementPortalService extends IntentService {
     public static final int MANAGEMENT_PORTAL_REGISTRATION_FAILED = 2;
     public static final int MANAGEMENT_PORTAL_REFRESH = 4;
     public static final int MANAGEMENT_PORTAL_REFRESH_FAILED = 5;
+    public static final int MANAGEMENT_PORTAL_REFRESH_TOKEN_RETRIEVED = 6;
+    public static final int MANAGEMENT_PORTAL_REFRESH_TOKEN_RETRIEVED_FAILED = 7;
+
 
     private static final Logger logger = LoggerFactory.getLogger(ManagementPortalService.class);
     private static final String REGISTER_SOURCE_ACTION = "org.radarcns.android.auth.ManagementPortalService.registerSourceAction";
     private static final String GET_STATE_ACTION = "org.radarcns.android.auth.ManagementPortalService.getStateAction";
     private static final String REFRESH_TOKEN_ACTION = "org.radarcns.android.auth.ManagementPortalService.refreshTokenAction";
+    private static final String GET_REFRESH_TOKEN_ACTION = "org.radarcns.android.auth.ManagementPortalService.getRefreshTokenAction";
     private static final String REFRESH_TOKEN_KEY = "org.radarcns.android.auth.ManagementPortalService.refreshToken";
+    private static final String REFRESH_TOKEN_KEY_URL = "org.radarcns.android.auth.ManagementPortalService.refreshTokenUrl";
     private static final String UPDATE_SUBJECT_KEY = "org.radarcns.android.auth.ManagementPortalService.updateSubject";
     public static final String REQUEST_FAILED_REASON = "org.radarcns.android.auth.ManagementPortalService.refreshFailedReason";
     public static final int REQUEST_FAILED_REASON_IO = 1;
@@ -113,6 +118,9 @@ public class ManagementPortalService extends IntentService {
                 case GET_STATE_ACTION:
                     isSuccessful = getState(extras);
                     break;
+                case GET_REFRESH_TOKEN_ACTION:
+                    isSuccessful = getRefreshToken(extras);
+                    break;
                 default:
                     logger.warn("Cannot complete action {}: action unknown", intent.getAction());
                     isSuccessful = false;
@@ -126,6 +134,45 @@ public class ManagementPortalService extends IntentService {
         } catch (IllegalArgumentException ex) {
             Crashlytics.logException(ex);
         }
+    }
+
+    private boolean getRefreshToken(Bundle extras) {
+        logger.info("retrieving refreshToken from url");
+        ResultReceiver receiver = extras.getParcelable(RESULT_RECEIVER_PROPERTY);
+        if (receiver == null) {
+            throw new IllegalArgumentException("ResultReceiver not set");
+        }
+
+        String refreshTokenUrl = extras.getParcelable(REFRESH_TOKEN_KEY_URL);
+        if (refreshTokenUrl == null) {
+            throw new IllegalArgumentException("RefreshTokenUrl not set");
+        }
+
+        Bundle result = new Bundle();
+
+        ConnectivityManager connManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager != null ? connManager.getActiveNetworkInfo() : null;
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            result.putInt(REQUEST_FAILED_REASON, REQUEST_FAILED_REASON_DISCONNECTED);
+            receiver.send(MANAGEMENT_PORTAL_REFRESH_TOKEN_RETRIEVED_FAILED, result);
+            return false;
+        }
+
+        try {
+            ensureClient();
+
+            AuthStringParser parser = new MetaTokenParser(authState);
+            authState = client.getRefreshToken(refreshTokenUrl, parser);
+            authState.addToBundle(result);
+            return refreshToken(result);
+        } catch (IOException e) {
+            logger.error("Failed to get access token", e);
+            result.putInt(REQUEST_FAILED_REASON, REQUEST_FAILED_REASON_IO);
+            receiver.send(MANAGEMENT_PORTAL_REFRESH_TOKEN_RETRIEVED_FAILED, result);
+        }
+
+        return true;
     }
 
     private boolean getState(Bundle extras) {
@@ -356,6 +403,17 @@ public class ManagementPortalService extends IntentService {
         extras.putString(REFRESH_TOKEN_KEY, refreshToken);
         extras.putParcelable(RESULT_RECEIVER_PROPERTY, receiver);
         extras.putBoolean(UPDATE_SUBJECT_KEY, updateSubject);
+        intent.putExtras(extras);
+        context.startService(intent);
+    }
+
+    /** Build an intent to create a request for the management portal. */
+    public static void requestRefreshToken(Context context, String refreshTokenUrl, ResultReceiver receiver) {
+        Intent intent = new Intent(context, ManagementPortalService.class);
+        intent.setAction(GET_REFRESH_TOKEN_ACTION);
+        Bundle extras = new Bundle();
+        extras.putString(REFRESH_TOKEN_KEY_URL, refreshTokenUrl);
+        extras.putParcelable(RESULT_RECEIVER_PROPERTY, receiver);
         intent.putExtras(extras);
         context.startService(intent);
     }
