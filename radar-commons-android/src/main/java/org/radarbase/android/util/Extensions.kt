29a5ccfabd434d67ca1c213e2c824ecda2d10557
Joris Borgdorff <github@blootsvoets.net>
2020-06-11 10:22:16 +0200

    Removed unused import

--- radar-commons-android/src/main/java/org/radarbase/android/util/Extensions.kt
+++ radar-commons-android/src/main/java/org/radarbase/android/util/Extensions.kt
@@ -1,6 +1,4 @@
 package org.radarbase.android.util
 
-import android.bluetooth.BluetoothAdapter
-
 fun String.takeTrimmedIfNotEmpty(): String? = trim { it <= ' ' }
             .takeUnless(String::isEmpty)
