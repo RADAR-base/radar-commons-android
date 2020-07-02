2bee9c19291c52050307cec8aea94485b29d6be4
Joris Borgdorff <github@blootsvoets.net>
2020-07-02 13:32:14 +0200

    More documentation

--- radar-commons-android/src/main/java/org/radarbase/android/data/serialization/SerializationFactory.kt
+++ radar-commons-android/src/main/java/org/radarbase/android/data/serialization/SerializationFactory.kt
@@ -4,8 +4,23 @@ import org.radarbase.data.Record
 import org.radarbase.topic.AvroTopic
 import org.radarbase.util.BackedObjectQueue
 
+/**
+ * Factory for serializer and deserializers for the data cache.
+ */
 interface SerializationFactory {
+    /**
+     * File extension to use with this serialization type. It should be unique across used
+     * serialization factories.
+     */
     val fileExtension: String
+
+    /**
+     * Creates a deserializer for a given topic.
+     */
     fun <K: Any, V: Any> createDeserializer(topic: AvroTopic<K, V>): BackedObjectQueue.Deserializer<Record<K, V>>
+
+    /**
+     * Creates a serializer for a given topic.
+     */
     fun <K: Any, V: Any> createSerializer(topic: AvroTopic<K, V>): BackedObjectQueue.Serializer<Record<K, V>>
 }
--- radar-commons-android/src/main/java/org/radarbase/android/data/serialization/TapeAvroSerializationFactory.kt
+++ radar-commons-android/src/main/java/org/radarbase/android/data/serialization/TapeAvroSerializationFactory.kt
@@ -6,35 +6,22 @@ import org.radarbase.data.Record
 import org.radarbase.topic.AvroTopic
 import org.radarbase.util.BackedObjectQueue
 
+/**
+ * Serialization for binary Avro records to a tape.
+ */
 class TapeAvroSerializationFactory: SerializationFactory {
     override val fileExtension: String = ".tape"
 
+    // The receiving end may have problems with non-numeric representations of floats, so they are not allowed.
     private val genericData: GenericData = object : GenericData(TapeAvroSerializationFactory::class.java.classLoader) {
-        override fun isFloat(`object`: Any?): Boolean {
-            return (`object` is Float
-                    && !`object`.isNaN()
-                    && !`object`.isInfinite())
-        }
-
-        override fun isDouble(`object`: Any?): Boolean {
-            return (`object` is Double
-                    && !`object`.isNaN()
-                    && !`object`.isInfinite())
-        }
+        override fun isFloat(datum: Any?): Boolean = datum is Float && datum.isFinite()
+        override fun isDouble(datum: Any?): Boolean = datum is Double && datum.isFinite()
     }
 
+    // The receiving end may have problems with non-numeric representations of floats, so they are not allowed.
     private val specificData: SpecificData = object : SpecificData(TapeAvroSerializationFactory::class.java.classLoader) {
-        override fun isFloat(`object`: Any?): Boolean {
-            return (`object` is Float
-                    && !`object`.isNaN()
-                    && !`object`.isInfinite())
-        }
-
-        override fun isDouble(`object`: Any?): Boolean {
-            return (`object` is Double
-                    && !`object`.isNaN()
-                    && !`object`.isInfinite())
-        }
+        override fun isFloat(datum: Any?): Boolean = datum is Float && datum.isFinite()
+        override fun isDouble(datum: Any?): Boolean = datum is Double && datum.isFinite()
     }
 
     override fun <K: Any, V: Any> createDeserializer(
