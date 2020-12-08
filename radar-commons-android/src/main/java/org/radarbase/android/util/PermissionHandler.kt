65312c82a7fee5a6ec80d2ff0550dd7c156a6d11
Joris Borgdorff <joris@thehyve.nl>
2020-12-08 14:01:27 +0100

    Better behaviour when requesting multiple permissions

--- radar-commons-android/src/main/java/org/radarbase/android/util/PermissionHandler.kt
+++ radar-commons-android/src/main/java/org/radarbase/android/util/PermissionHandler.kt
@@ -6,102 +6,126 @@ import android.Manifest.permission.SYSTEM_ALERT_WINDOW
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
+import android.app.AppOpsManager
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.PowerManager
+import android.os.Process
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleService
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import org.radarbase.android.R
 import org.radarbase.android.RadarService
+import org.radarbase.android.RadarService.Companion.applySystemService
+import org.radarbase.android.source.SourceProvider
 import org.slf4j.LoggerFactory
 
 
 open class PermissionHandler(
     private val activity: AppCompatActivity,
     private val mHandler: SafeHandler,
-    private val requestPermissionTimeoutMs: Long
+    private val requestPermissionTimeoutMs: Long,
 ) {
     private val broadcaster = LocalBroadcastManager.getInstance(activity)
 
-    private var needsPermissions: MutableSet<String> = HashSet()
+    private val needsPermissions: MutableSet<String> = HashSet()
     private val isRequestingPermissions: MutableSet<String> = HashSet()
     private var isRequestingPermissionsTime = java.lang.Long.MAX_VALUE
+    private var requestFuture: SafeHandler.HandlerFuture? = null
 
     private fun onPermissionRequestResult(permission: String, granted: Boolean) {
         mHandler.execute {
             needsPermissions.remove(permission)
 
-            val result = if (granted) PackageManager.PERMISSION_GRANTED else PackageManager.PERMISSION_DENIED
+            val result = if (granted) PERMISSION_GRANTED else PERMISSION_DENIED
             broadcaster.send(RadarService.ACTION_PERMISSIONS_GRANTED) {
                 putExtra(RadarService.EXTRA_PERMISSIONS, arrayOf(Context.LOCATION_SERVICE))
                 putExtra(RadarService.EXTRA_GRANT_RESULTS, intArrayOf(result))
             }
 
             isRequestingPermissions.remove(permission)
-            checkPermissions()
+            requestPermissions()
         }
     }
 
-    private fun checkPermissions() {
+    private fun requestPermissions() {
         mHandler.executeReentrant {
-            val currentlyNeeded = needsPermissions - isRequestingPermissions
-            when {
-                needsPermissions.isEmpty() -> {
-                    broadcaster.send(RadarService.ACTION_PERMISSIONS_GRANTED) {
-                        putExtra(RadarService.EXTRA_PERMISSIONS, arrayOfNulls<String>(0))
-                        putExtra(RadarService.EXTRA_GRANT_RESULTS, IntArray(0))
-                    }
+            requestFuture?.cancel()
+            requestFuture = mHandler.delay(500L) {
+                doRequestPermission()
+                requestFuture = null
+            }
+        }
+    }
+
+    private fun doRequestPermission() {
+        val externallyGrantedPermissions = needsPermissions.filter { activity.isPermissionGranted(it) }
+
+        if (externallyGrantedPermissions.isNotEmpty()) {
+            broadcaster.send(RadarService.ACTION_PERMISSIONS_GRANTED) {
+                putExtra(RadarService.EXTRA_PERMISSIONS,
+                    externallyGrantedPermissions.toTypedArray())
+                putExtra(RadarService.EXTRA_GRANT_RESULTS,
+                    IntArray(externallyGrantedPermissions.size) { PERMISSION_GRANTED })
+            }
+            isRequestingPermissions -= externallyGrantedPermissions
+            needsPermissions -= externallyGrantedPermissions
+        }
+
+        val currentlyNeeded = HashSet(needsPermissions)
+        currentlyNeeded -= isRequestingPermissions
+
+        when {
+            Context.LOCATION_SERVICE in currentlyNeeded -> {
+                addRequestingPermissions(Context.LOCATION_SERVICE)
+                requestLocationProvider()
+            }
+            SYSTEM_ALERT_WINDOW in currentlyNeeded -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    addRequestingPermissions(SYSTEM_ALERT_WINDOW)
+                    requestSystemWindowPermissions()
                 }
-                currentlyNeeded.isEmpty() -> {
+            }
+            RadarService.PACKAGE_USAGE_STATS_COMPAT in currentlyNeeded -> {
+                addRequestingPermissions(RadarService.PACKAGE_USAGE_STATS_COMPAT)
+                requestPackageUsageStats()
+            }
+            RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT in currentlyNeeded -> {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    addRequestingPermissions(RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT)
+                    requestDisableBatteryOptimization()
+                } else {
+                    needsPermissions.remove(RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT)
                 }
-                Context.LOCATION_SERVICE in currentlyNeeded -> {
-                    addRequestingPermissions(Context.LOCATION_SERVICE)
-                    requestLocationProvider()
+            }
+            else -> {
+                if (
+                    ACCESS_COARSE_LOCATION in currentlyNeeded
+                    || ACCESS_FINE_LOCATION in currentlyNeeded
+                ) {
+                    currentlyNeeded -= RadarService.ACCESS_BACKGROUND_LOCATION_COMPAT
                 }
-                SYSTEM_ALERT_WINDOW in currentlyNeeded -> {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        addRequestingPermissions(SYSTEM_ALERT_WINDOW)
-                        requestSystemWindowPermissions()
-                    }
-                }
-                RadarService.PACKAGE_USAGE_STATS_COMPAT in currentlyNeeded -> {
-                    addRequestingPermissions(RadarService.PACKAGE_USAGE_STATS_COMPAT)
-                    requestPackageUsageStats()
-                }
-                RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT in currentlyNeeded -> {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        addRequestingPermissions(RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT)
-                        requestDisableBatteryOptimization()
-                    } else {
-                        needsPermissions.remove(RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT)
-                    }
-                }
-                else -> {
-                    val compatiblePermissions = if (
-                            ACCESS_COARSE_LOCATION in needsPermissions
-                            || ACCESS_FINE_LOCATION in needsPermissions) {
-                        currentlyNeeded - RadarService.ACCESS_BACKGROUND_LOCATION_COMPAT
-                    } else {
-                        currentlyNeeded
-                    }
-                    addRequestingPermissions(compatiblePermissions)
-                    try {
-                        ActivityCompat.requestPermissions(
-                            activity,
-                            compatiblePermissions.toTypedArray(),
-                            REQUEST_ENABLE_PERMISSIONS,
-                        )
-                    } catch (ex: IllegalStateException) {
-                        logger.warn("Cannot request permission on closing activity")
-                    }
+                addRequestingPermissions(currentlyNeeded)
+                try {
+                    ActivityCompat.requestPermissions(
+                        activity,
+                        currentlyNeeded.toTypedArray(),
+                        REQUEST_ENABLE_PERMISSIONS,
+                    )
+                } catch (ex: IllegalStateException) {
+                    logger.warn("Cannot request permission on closing activity")
                 }
             }
         }
@@ -123,7 +147,7 @@ open class PermissionHandler(
             isRequestingPermissionsTime = System.currentTimeMillis()
             mHandler.delay(requestPermissionTimeoutMs) {
                 resetRequestingPermission()
-                checkPermissions()
+                requestPermissions()
             }
         }
     }
@@ -242,13 +266,23 @@ open class PermissionHandler(
         }
     }
 
+
     fun replaceNeededPermissions(newPermissions: Array<out String>?) {
         newPermissions ?: return
         mHandler.execute {
-            needsPermissions = mutableSetOf(*newPermissions)
-            checkPermissions()
+            needsPermissions.clear()
+
+            needsPermissions += ArrayList<String>(newPermissions.size + 1).apply {
+                this += newPermissions
+                if (ACCESS_FINE_LOCATION in this || ACCESS_COARSE_LOCATION in this) {
+                    this += LifecycleService.LOCATION_SERVICE
+                }
+            }.filterNot { activity.isPermissionGranted(it) }
+
+            requestPermissions()
         }
     }
+
 
     fun permissionsGranted(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == REQUEST_ENABLE_PERMISSIONS) {
@@ -287,5 +321,23 @@ open class PermissionHandler(
         private const val USAGE_REQUEST_CODE = 232619695 and 0xFFFF
         private const val BATTERY_OPT_CODE = 232619696 and 0xFFFF
         private const val ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 232619697 and 0xFFFF
+
+        fun Context.isPermissionGranted(permission: String): Boolean = when (permission) {
+            LifecycleService.LOCATION_SERVICE -> applySystemService<LocationManager>(Context.LOCATION_SERVICE) { locationManager ->
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                        || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+            } ?: true
+            RadarService.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS_COMPAT -> applySystemService<PowerManager>(Context.POWER_SERVICE) { powerManager ->
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                        || powerManager.isIgnoringBatteryOptimizations(applicationContext.packageName)
+            } ?: true
+            RadarService.PACKAGE_USAGE_STATS_COMPAT -> applySystemService<AppOpsManager>(Context.APP_OPS_SERVICE) { appOps ->
+                @Suppress("DEPRECATION")
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                        || AppOpsManager.MODE_ALLOWED == appOps.checkOpNoThrow("android:get_usage_stats", Process.myUid(), packageName)
+            } ?: true
+            SYSTEM_ALERT_WINDOW -> Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)
+            else -> PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, permission)
+        }
     }
 }
