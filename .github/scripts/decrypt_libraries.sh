2eb446e584915fb5f84f4d243858809531b73cb9
Joris Borgdorff <joris@thehyve.nl>
2022-06-27 12:00:16 +0200

    Fix build for external PRs

--- .github/scripts/decrypt_libraries.sh
+++ .github/scripts/decrypt_libraries.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-gpg --quiet --batch --yes --decrypt --passphrase="$E4LINK_PASSPHRASE" \
-  --output plugins/radar-android-empatica/libs/E4link-1.0.0.aar .github/libs/E4link-1.0.0.aar.gpg
+if [ -z "$E4LINK_PASSPHRASE" ]; then
+  touch plugins/radar-android-empatica/gradle.skip
+else
+  gpg --quiet --batch --yes --decrypt --passphrase="$E4LINK_PASSPHRASE" \
+    --output plugins/radar-android-empatica/libs/E4link-1.0.0.aar .github/libs/E4link-1.0.0.aar.gpg
+fi
