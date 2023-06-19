2a187e794cb69af21f75ed0a046bfe28ade9d474
Joris Borgdorff <joris@thehyve.nl>
2023-06-19 14:51:08 +0200

    Add configured types to source manager

--- plugins/radar-android-health-connect/src/main/kotlin/org/radarbase/passive/google/healthconnect/HealthConnectService.kt
+++ plugins/radar-android-health-connect/src/main/kotlin/org/radarbase/passive/google/healthconnect/HealthConnectService.kt
@@ -1,8 +1,10 @@
 package org.radarbase.passive.google.healthconnect
 
+import org.radarbase.android.config.SingleRadarConfiguration
 import org.radarbase.android.source.BaseSourceState
 import org.radarbase.android.source.SourceManager
 import org.radarbase.android.source.SourceService
+import org.radarbase.passive.google.healthconnect.HealthConnectProvider.Companion.toHealthConnectTypes
 
 class HealthConnectService : SourceService<BaseSourceState>() {
     override val defaultState: BaseSourceState
@@ -10,4 +12,13 @@ class HealthConnectService : SourceService<BaseSourceState>() {
 
     override fun createSourceManager(): SourceManager<BaseSourceState> =
         HealthConnectSourceManager(this)
+
+    override fun configureSourceManager(
+        manager: SourceManager<BaseSourceState>,
+        config: SingleRadarConfiguration
+    ) {
+        manager as HealthConnectSourceManager
+        manager.dataTypes = config.getString(HealthConnectProvider.HEALTH_CONNECT_DATA_TYPES, "")
+            .toHealthConnectTypes()
+    }
 }
