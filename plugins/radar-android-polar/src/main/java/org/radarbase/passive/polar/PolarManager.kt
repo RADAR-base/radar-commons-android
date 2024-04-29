27c99d9fb622d7b59c6194d1a0a5d16ef3411e15
Famke Schulting <fschulting@gmail.com>
2024-04-29 14:47:36 +0200

    Add Acc and Ecg Topics

--- plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarManager.kt
+++ plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarManager.kt
@@ -14,6 +14,7 @@ import com.polar.sdk.api.PolarBleApiCallback
 import com.polar.sdk.api.PolarBleApiDefaultImpl.defaultImplementation
 import com.polar.sdk.api.model.*
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.Disposable
 import org.radarbase.android.data.DataCache
@@ -29,15 +30,10 @@ class PolarManager(
     private val applicationContext: Context
 ) : AbstractSourceManager<PolarService, PolarState>(polarService) {
 
-
-//    private val heartRateTopic: DataCache<ObservationKey, PhoneStepCount> = createCache("android_phone_step_count", PhoneStepCount())
-
-//    private val accelerationTopic: DataCache<ObservationKey, PolarAcceleration> = createCache("android_polar_acceleration", PolarAcceleration())
+    private val accelerationTopic: DataCache<ObservationKey, PolarAcceleration> = createCache("android_polar_acceleration", PolarAcceleration())
     private val batteryLevelTopic: DataCache<ObservationKey, PolarBatteryLevel> = createCache("android_polar_battery_level", PolarBatteryLevel())
-//    private val ecgTopic: DataCache<ObservationKey, PolarEcg> = createCache("android_polar_ecg", PolarEcg())
-//    private val gyroscopeTopic: DataCache<ObservationKey, PolarGyroscope> = createCache("android_polar_gyroscope", PolarGyroscope())
+    private val ecgTopic: DataCache<ObservationKey, PolarEcg> = createCache("android_polar_ecg", PolarEcg())
     private val heartRateTopic: DataCache<ObservationKey, PolarHeartRate> = createCache("android_polar_heart_rate", PolarHeartRate())
-//    private val magnetometerTopic: DataCache<ObservationKey, PolarMagnetometer> = createCache("android_polar_magnetometer", PolarMagnetometer())
 //    private val ppIntervalTopic: DataCache<ObservationKey, PolarPpInterval> = createCache("android_polar_pp_interval", PolarPpInterval())
 //    private val temperatureTopic: DataCache<ObservationKey, PolarTemperature> = createCache("android_polar_temperature", PolarTemperature())
 
@@ -52,12 +48,7 @@ class PolarManager(
     private var hrDisposable: Disposable? = null
     private var ecgDisposable: Disposable? = null
     private var accDisposable: Disposable? = null
-    private var gyrDisposable: Disposable? = null
-    private var magDisposable: Disposable? = null
     private var ppiDisposable: Disposable? = null
-    private var ppgDisposable: Disposable? = null
-
-
     companion object {
         private const val TAG = "POLAR"
 
@@ -71,7 +62,6 @@ class PolarManager(
     @SuppressLint("WakelockTimeout")
     override fun start(acceptableIds: Set<String>) {
 
-//        disconnectToPolarSDK()
         status = SourceStatusListener.Status.READY // blue loading
         Log.d(TAG, "RB Device name is currently $deviceId")
 
@@ -88,7 +78,6 @@ class PolarManager(
         }
 
     }
-
     fun connectToPolarSDK() {
         api = defaultImplementation(
             applicationContext,
@@ -142,11 +131,13 @@ class PolarManager(
                 if (isDeviceConnected) {
                     Log.d(TAG, "Feature ready $feature for $deviceId")
 
+
                     when (feature) {
                         PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_ONLINE_STREAMING -> {
                             streamHR()
+                            streamEcg()
                             streamAcc()
-                            streamPpi()
+//                            streamPpi()
                         }
 
                         else -> {
@@ -261,82 +252,66 @@ class PolarManager(
 //        }
 //    }
 
-
-
-//    fun streamEcg() {
-//        val isDisposed = ecgDisposable?.isDisposed ?: true
-//        if (isDisposed) {
-//            ecgDisposable = requestStreamSettings(deviceId, PolarBleApi.PolarDeviceDataType.ECG)
-//                .flatMap { settings: PolarSensorSetting ->
-//                    api.startEcgStreaming(deviceId, settings)
-//                }
-//                .subscribe(
-//                    { polarEcgData: PolarEcgData ->
-//                        for (data in polarEcgData.samples) {
-//                            Log.d(TAG, "    yV: ${data.voltage} timeStamp: ${data.timeStamp}")
-//                        }
-//                    },
-//                    { error: Throwable ->
-//                        Log.e(TAG, "ECG stream failed. Reason $error")
-//                    },
-//                    { Log.d(TAG, "ECG stream complete") }
-//                )
-//        } else {
-//            // NOTE stops streaming if it is "running"
-//            ecgDisposable?.dispose()
-//        }
-//    }
-
-//    fun streamGyro() {
-//        val isDisposed = gyrDisposable?.isDisposed ?: true
-//        if (isDisposed) {
-//            gyrDisposable =
-//                requestStreamSettings(deviceId, PolarBleApi.PolarDeviceDataType.GYRO)
-//                    .flatMap { settings: PolarSensorSetting ->
-//                        api.startGyroStreaming(deviceId, settings)
-//                    }
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(
-//                        { polarGyroData: PolarGyroData ->
-//                            for (data in polarGyroData.samples) {
-//                                Log.d(TAG, "GYR    x: ${data.x} y: ${data.y} z: ${data.z} timeStamp: ${data.timeStamp}")
-//                            }
-//                        },
-//                        { error: Throwable ->
-//                            Log.e(TAG, "GYR stream failed. Reason $error")
-//                        },
-//                        { Log.d(TAG, "GYR stream complete") }
-//                    )
-//        } else {
-//            // NOTE dispose will stop streaming if it is "running"
-//            gyrDisposable?.dispose()
-//        }
-//    }
-
+        fun streamEcg() {
+            val isDisposed = ecgDisposable?.isDisposed ?: true
+            if (isDisposed) {
+                val settingMap = mapOf(
+                    PolarSensorSetting.SettingType.SAMPLE_RATE to 103,
+                    PolarSensorSetting.SettingType.RESOLUTION to 14
+                )
+                val ecgSettings = PolarSensorSetting(settingMap)
+                deviceId?.let { deviceId ->
+                    ecgDisposable = api.startEcgStreaming(deviceId, ecgSettings)
+                        .subscribe(
+                            { polarEcgData: PolarEcgData ->
+                                for (data in polarEcgData.samples) {
+                                    Log.d(TAG, "ECG   yV: ${data.voltage} timeStamp: ${data.timeStamp}")
+                                    send(
+                                        ecgTopic,
+                                        PolarEcg(
+                                            getTime(),
+                                            getTime(),
+                                            data.voltage
+                                        )
+                                    )
+                                }
+                            },
+                            { error: Throwable ->
+                                Log.e(TAG, "ECG stream failed. Reason $error")
+                            },
+                            { Log.d(TAG, "ECG stream complete") }
+                        )
+                }
+            } else {
+                // NOTE stops streaming if it is "running"
+                ecgDisposable?.dispose()
+            }
+        }
     fun streamAcc() {
         val isDisposed = accDisposable?.isDisposed ?: true
         if (isDisposed) {
             val settingMap = mapOf(
-                PolarSensorSetting.SettingType.SAMPLE_RATE to 52,
-                PolarSensorSetting.SettingType.RESOLUTION to 16,
-                PolarSensorSetting.SettingType.RANGE to 8,
-                PolarSensorSetting.SettingType.CHANNELS to 3
+                PolarSensorSetting.SettingType.SAMPLE_RATE to 25, // [50, 100, 200, 25]
+                PolarSensorSetting.SettingType.RESOLUTION to 16, // [16]
+                PolarSensorSetting.SettingType.RANGE to 2 // [2, 4, 8]
             )
-            val sensorSetting = PolarSensorSetting(settingMap)
-
-//            accDisposable = requestStreamSettings(deviceId, PolarBleApi.PolarDeviceDataType.ACC)
-//                .flatMap { settings: PolarSensorSetting ->
-//                    api.startAccStreaming(deviceId, settings)
-//                    api.startAccStreaming(deviceId, settings)
-//                }
-
-            accDisposable = deviceId?.let {
-                api.startAccStreaming(it, sensorSetting)
-                    .observeOn(AndroidSchedulers.mainThread())
+            val accSettings = PolarSensorSetting(settingMap)
+            deviceId?.let { deviceId ->
+                accDisposable = api.startAccStreaming(deviceId, accSettings)
                     .subscribe(
                         { polarAccelerometerData: PolarAccelerometerData ->
                             for (data in polarAccelerometerData.samples) {
                                 Log.d(TAG, "ACC    x: ${data.x} y: ${data.y} z: ${data.z} timeStamp: ${data.timeStamp}")
+                                send(
+                                    accelerationTopic,
+                                    PolarAcceleration(
+                                        getTime(),
+                                        getTime(),
+                                        data.x,
+                                        data.y,
+                                        data.z
+                                    )
+                                )
                             }
                         },
                         { error: Throwable ->
@@ -352,59 +327,6 @@ class PolarManager(
             accDisposable?.dispose()
         }
     }
-
-//    fun streamMag() {
-//        val isDisposed = magDisposable?.isDisposed ?: true
-//        if (isDisposed) {
-//            magDisposable =
-//                requestStreamSettings(deviceId, PolarBleApi.PolarDeviceDataType.MAGNETOMETER)
-//                    .flatMap { settings: PolarSensorSetting ->
-//                        api.startMagnetometerStreaming(deviceId, settings)
-//                    }
-//                    .observeOn(AndroidSchedulers.mainThread())
-//                    .subscribe(
-//                        { polarMagData: PolarMagnetometerData ->
-//                            for (data in polarMagData.samples) {
-//                                Log.d(TAG, "MAG    x: ${data.x} y: ${data.y} z: ${data.z} timeStamp: ${data.timeStamp}")
-//                            }
-//                        },
-//                        { error: Throwable ->
-//                            Log.e(TAG, "MAGNETOMETER stream failed. Reason $error")
-//                        },
-//                        { Log.d(TAG, "MAGNETOMETER stream complete") }
-//                    )
-//        } else {
-//            // NOTE dispose will stop streaming if it is "running"
-//            magDisposable!!.dispose()
-//        }
-//    }
-
-//    fun streamPpg() {
-//        val isDisposed = ppgDisposable?.isDisposed ?: true
-//        if (isDisposed) {
-//            ppgDisposable =
-//                requestStreamSettings(deviceId, PolarBleApi.PolarDeviceDataType.PPG)
-//                    .flatMap { settings: PolarSensorSetting ->
-//                        api.startPpgStreaming(deviceId, settings)
-//                    }
-//                    .subscribe(
-//                        { polarPpgData: PolarPpgData ->
-//                            if (polarPpgData.type == PolarPpgData.PpgDataType.PPG3_AMBIENT1) {
-//                                for (data in polarPpgData.samples) {
-//                                    Log.d(TAG, "PPG    ppg0: ${data.channelSamples[0]} ppg1: ${data.channelSamples[1]} ppg2: ${data.channelSamples[2]} ambient: ${data.channelSamples[3]} timeStamp: ${data.timeStamp}")
-//                                }
-//                            }
-//                        },
-//                        { error: Throwable ->
-//                            Log.e(TAG, "PPG stream failed. Reason $error")
-//                        },
-//                        { Log.d(TAG, "PPG stream complete") }
-//                    )
-//        } else {
-//            // NOTE dispose will stop streaming if it is "running"
-//            ppgDisposable?.dispose()
-//        }
-//    }
 
     fun streamPpi() {
         val isDisposed = ppiDisposable?.isDisposed ?: true
@@ -432,54 +354,6 @@ class PolarManager(
             ppiDisposable?.dispose()
         }
     }
-
-//    private fun requestStreamSettings(identifier: String, feature: PolarBleApi.PolarDeviceDataType): Flowable<PolarSensorSetting> {
-//        val availableSettings = api.requestStreamSettings(identifier, feature)
-//        val allSettings = api.requestFullStreamSettings(identifier, feature)
-//            .onErrorReturn { error: Throwable ->
-//                Log.w(TAG, "Full stream settings are not available for feature $feature. REASON: $error")
-//                PolarSensorSetting(emptyMap())
-//            }
-//
-//        return Single.zip(availableSettings, allSettings) { available: PolarSensorSetting, all: PolarSensorSetting ->
-//            if (available.settings.isEmpty()) {
-//                throw Throwable("Settings are not available")
-//            } else {
-//                Log.d(TAG, "Feature $feature available settings ${available.settings}")
-//                Log.d(TAG, "Feature $feature all settings ${all.settings}")
-//                return@zip PolarSensorSetting(all.settings)
-//            }
-//        }
-//            .toFlowable()
-//            .observeOn(AndroidSchedulers.mainThread())
-//    }
-
-//    private fun requestStreamSettings(identifier: String, feature: PolarBleApi.PolarDeviceDataType): Flowable<PolarSensorSetting> {
-//        val availableSettings = api.requestStreamSettings(identifier, feature)
-//        val allSettings = api.requestFullStreamSettings(identifier, feature)
-//            .onErrorReturn { error: Throwable ->
-//                Log.w(TAG, "Full stream settings are not available for feature $feature. REASON: $error")
-//                PolarSensorSetting(emptyMap())
-//            }
-//        return Single.zip(availableSettings, allSettings) { available: PolarSensorSetting, all: PolarSensorSetting ->
-//            if (available.settings.isEmpty()) {
-//                throw Throwable("Settings are not available")
-//            } else {
-//                Log.d(TAG, "Feature " + feature + " available settings " + available.settings)
-//                Log.d(TAG, "Feature " + feature + " all settings " + all.settings)
-//                return@zip android.util.Pair(available, all)
-//            }
-//        }
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .toFlowable()
-//            .flatMap { sensorSettings: android.util.Pair<PolarSensorSetting, PolarSensorSetting> ->
-//                DialogUtility.showAllSettingsDialog(
-//                    this@MainActivity,
-//                    sensorSettings.first.settings,
-//                    sensorSettings.second.settings
-//                ).toFlowable()
-//            }
-//    }
 
 }
 
