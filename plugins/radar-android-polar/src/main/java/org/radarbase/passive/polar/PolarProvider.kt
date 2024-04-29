b039e4b006c3dcf95f9086fd125a721dcf2f64ad
Famke Schulting <fschulting@gmail.com>
2024-04-29 14:48:05 +0200

    Add necessary permissions and turn Filter on

--- plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarProvider.kt
+++ plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarProvider.kt
@@ -1,5 +1,8 @@
 package org.radarbase.passive.polar
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import org.radarbase.android.BuildConfig
 import org.radarbase.android.RadarService
 import org.radarbase.android.source.SourceProvider
@@ -16,11 +19,26 @@ open class PolarProvider(radarService: RadarService) : SourceProvider<PolarState
 
     override val description: String
         get() = radarService.getString(R.string.polarSensorsDescription)
+    override val hasDetailView = true
 
     override val displayName: String
         get() = radarService.getString(R.string.polarDisplayName)
 
-    override val permissionsNeeded: List<String> = emptyList()
+    override val permissionsNeeded = buildList {
+        add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        add(Manifest.permission.ACCESS_FINE_LOCATION)
+        add(Manifest.permission.BLUETOOTH)
+        add(Manifest.permission.BLUETOOTH_ADMIN)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            add(Manifest.permission.BLUETOOTH_SCAN)
+            add(Manifest.permission.BLUETOOTH_CONNECT)
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+    }
+
+    override val featuresNeeded = listOf(PackageManager.FEATURE_BLUETOOTH, PackageManager.FEATURE_BLUETOOTH_LE)
 
     override val sourceProducer: String = PRODUCER
 
@@ -28,6 +46,7 @@ open class PolarProvider(radarService: RadarService) : SourceProvider<PolarState
 
     override val version: String = BuildConfig.VERSION_NAME
 
+    override val isFilterable = true
     companion object {
         const val PRODUCER = "Polar"
         const val MODEL = "Generic"
