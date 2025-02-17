1bf36dd17226ea811da6c789d823ae0acc9aa86c
this-Aditya <adityamishra2217@gmail.com>
2025-02-17 13:17:30 +0530

    Implemented paging-data-adapters for showing network and plugin logs

---
+++ plugins/radar-android-application-status/src/main/java/org/radarbase/monitor/application/ui/adapter/NetworkStatusPagingAdapter.kt
@@ -0,0 +1,63 @@
+package org.radarbase.monitor.application.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import org.radarbase.android.storage.entity.NetworkStatusLog
+import org.radarbase.monitor.application.R
+import org.radarbase.monitor.application.databinding.ItemLogRowBinding
+import org.radarbase.monitor.application.utils.dateTimeFromInstant
+
+class NetworkStatusPagingAdapter(
+    private val context: Context,
+    private val onClickAction: (NetworkStatusLog) -> Unit
+) : PagingDataAdapter<NetworkStatusLog, NetworkStatusPagingAdapter.NetworkStatusViewHolder>(
+    DIFF_CALLBACK
+) {
+
+    inner class NetworkStatusViewHolder(private val binding: ItemLogRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(log: NetworkStatusLog) {
+            binding.tvStatusInfo.text = context.getString(
+                R.string.network_status_info,
+                log.connectionState.name,
+                dateTimeFromInstant(log.time)
+            )
+
+            binding.root.setOnClickListener {
+                log.apply(onClickAction)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: NetworkStatusViewHolder, position: Int) {
+        val log = getItem(position) ?: return
+        holder.bind(log)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NetworkStatusViewHolder {
+        val binding = ItemLogRowBinding.inflate(LayoutInflater.from(context), parent, false)
+        return NetworkStatusViewHolder(binding)
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<NetworkStatusLog>() {
+            override fun areItemsTheSame(
+                oldItem: NetworkStatusLog,
+                newItem: NetworkStatusLog
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: NetworkStatusLog,
+                newItem: NetworkStatusLog
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
---
+++ plugins/radar-android-application-status/src/main/java/org/radarbase/monitor/application/ui/adapter/SourceStatusPagingAdapter.kt
@@ -0,0 +1,63 @@
+package org.radarbase.monitor.application.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import org.radarbase.android.storage.entity.SourceStatusLog
+import org.radarbase.monitor.application.R
+import org.radarbase.monitor.application.databinding.ItemLogRowBinding
+import org.radarbase.monitor.application.utils.dateTimeFromInstant
+
+class SourceStatusPagingAdapter(
+    private val context: Context,
+    private val onClickAction: (SourceStatusLog) -> Unit
+) : PagingDataAdapter<SourceStatusLog, SourceStatusPagingAdapter.SourceStatusViewHolder>(
+    DIFF_CALLBACK
+) {
+
+    inner class SourceStatusViewHolder(private val binding: ItemLogRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(log: SourceStatusLog) {
+            binding.tvStatusInfo.text = context.getString(
+                R.string.source_status_info,
+                log.sourceStatus.name,
+                dateTimeFromInstant(log.time)
+            )
+
+            binding.root.setOnClickListener {
+                log.apply(onClickAction)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: SourceStatusViewHolder, position: Int) {
+        val log = getItem(position) ?: return
+        holder.bind(log)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SourceStatusViewHolder {
+        val binding = ItemLogRowBinding.inflate(LayoutInflater.from(context), parent, false)
+        return SourceStatusViewHolder(binding)
+    }
+
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<SourceStatusLog>() {
+            override fun areItemsTheSame(
+                oldItem: SourceStatusLog,
+                newItem: SourceStatusLog
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: SourceStatusLog,
+                newItem: SourceStatusLog
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
