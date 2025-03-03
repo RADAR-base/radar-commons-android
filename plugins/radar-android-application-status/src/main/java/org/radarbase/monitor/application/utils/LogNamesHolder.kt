a4a54c9769cfb834da1ab3de6cbd66a7448d31b4
this-Aditya <adityamishra2217@gmail.com>
2025-03-03 12:49:40 +0530

    Deleted adapter classes from the plugin as it is not required

--- plugins/radar-android-application-status/src/main/java/org/radarbase/monitor/application/utils/LogNamesHolder.kt
+++
@@ -1,6 +0,0 @@
-package org.radarbase.monitor.application.utils
-
-enum class LogNamesHolder(val alias: String) {
-    APPLICATION_PLUGIN_STATUS("Application Plugin Status"),
-    APPLICATION_NETWORK_STATUS("Application Network Status")
-}
