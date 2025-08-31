4fe8affb53d2ff68cdbd7a75a78b42d603748a3c
this-Aditya <adityamishra2217@gmail.com>
2025-08-31 23:37:38 +0530

    Sep Qr data parser added

---
+++ radar-commons-android/src/main/java/org/radarbase/android/auth/sep/SepQrParser.kt
@@ -0,0 +1,43 @@
+package org.radarbase.android.auth.sep
+
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import org.json.JSONException
+import org.json.JSONObject
+import org.radarbase.android.auth.AppAuthState
+import org.radarbase.android.auth.AuthService.Companion.BASE_URL_PROPERTY
+import org.radarbase.android.auth.AuthStringParser
+import org.radarbase.android.auth.sep.SEPClient.Companion.SEP_REFRESH_TOKEN_PROPERTY
+import org.radarbase.android.auth.sep.SEPLoginManager.Companion.SOURCE_TYPE_SEP
+import java.io.IOException
+
+class SepQrParser(private val state: AppAuthState, private val referrer: String) : AuthStringParser {
+
+    override fun parse(value: String): AppAuthState {
+        try {
+            val json = JSONObject(value)
+            val serverUrl = referrer.toHttpUrlOrNull() ?: throw IOException("Referrer is null, can't get base url")
+            val baseUrl = buildString {
+                append(serverUrl.scheme)
+                append("://")
+                append(serverUrl.host)
+                val port = serverUrl.port
+                if (port != -1) {
+                    append(":")
+                    append(port)
+                }
+            }
+
+            return state.alter {
+                attributes[SEP_REFRESH_TOKEN_PROPERTY] = json.getString("refresh_token")
+                attributes[BASE_URL_PROPERTY] = baseUrl
+                needsRegisteredSources = true
+                authenticationSource = SOURCE_TYPE_SEP
+
+            }
+        } catch (ex: JSONException) {
+            throw IOException("Failed to parse json string $value", ex)
+        }
+    }
+
+
+}
