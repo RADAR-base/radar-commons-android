845da6d56391bcfd2fc58d13bcd7d756f90a416c
this-Aditya <adityamishra2217@gmail.com>
2026-06-14 18:27:42 +0530

    Create a new state for polar UI

--- plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarState.kt
+++ plugins/radar-android-polar/src/main/java/org/radarbase/passive/polar/PolarState.kt
@@ -2,9 +2,6 @@ package org.radarbase.passive.polar
 
 import org.radarbase.android.source.BaseSourceState
 
-/**
- * The status on a single point in time
- */
 class PolarState : BaseSourceState() {
     override val acceleration = floatArrayOf(Float.NaN, Float.NaN, Float.NaN)
     @set:Synchronized
@@ -12,10 +9,34 @@ class PolarState : BaseSourceState() {
 
     override val hasAcceleration: Boolean = true
 
+    @Volatile
+    var deviceName: String? = null
+
+    /**
+     * Whether data is actively streaming.
+     */
+    @Volatile
+    var isCollecting: Boolean = false
+
+    /**
+     * Whether the user has asked to connect (tapped Connect).
+     */
+    @Volatile
+    var connectionRequested: Boolean = false
+
+    @Volatile
+    var polarController: PolarController? = null
+
     @Synchronized
     fun setAcceleration(x: Float, y: Float, z: Float) {
         this.acceleration[0] = x
         this.acceleration[1] = y
         this.acceleration[2] = z
+    }
+
+    interface PolarController {
+        fun connectDevice()
+        fun startCollecting()
+        fun stopCollecting()
     }
 }
